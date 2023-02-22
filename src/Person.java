27f25cd6a03b4a536179299156e9180431407a65
JosiahElder <@JosiahElder>
2023-02-22 01:18:36 -0800

    Next Commit

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,32 @@
 class Person
 {
+    private String firstName, lastName;
+    private boolean isInvited;
 
+    public Person(String firstName, String lastName, boolean isInvited)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isInvited = isInvited;
+    }
+
+    public boolean isInvited()
+    {
+        return isInvited;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getFullName()
+    {
+        return firstName + " " + lastName;
+    }
 }
