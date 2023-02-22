5b9d0835b3deafa5aa67cf816d8a16c06ceeb431
JosiahElder <@JosiahElder>
2023-02-22 01:21:42 -0800

    Next Commit

--- src/Friend.java
+++ src/Friend.java
@@ -1,4 +1,19 @@
 class Friend extends Person
 {
+    private String foodToBring;
 
+    public Friend(String firstName, String lastName, boolean isInvited, String foodToBring)
+    {
+        super(firstName, lastName, isInvited);
+        this.foodToBring = foodToBring;
+    }
+
+    public String toString()
+    {
+        String invitedString = isInvited() ?
+                "They are invited to the party." :
+                "They are NOT invited to the party.";
+
+        return getFullName() + " is bringing " + foodToBring + ". " + invitedString;
+    }
 }
