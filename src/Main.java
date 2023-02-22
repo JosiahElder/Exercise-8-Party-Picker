2015e9d3abf61767f593d1e42b7934f9b965c77f
JosiahElder <@JosiahElder>
2023-02-22 01:28:36 -0800

    Next Commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.JOptionPane;
 import java.util.ArrayList;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        ArrayList<Friend> friendList;
+
+        JOptionPane.showMessageDialog(null, "Welcome to the Party Picker.\n\n" +
+                "Enter person's name and the type of food they're likely to bring, \n" +
+                "then indicate whether they are actually invited to the party or not.");
 
     }
 }
