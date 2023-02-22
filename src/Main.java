f8a716fc36955ba0333105ab4b9641bfcf820559
JosiahElder <@JosiahElder>
2023-02-22 01:40:06 -0800

    Next Commit

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,25 @@ import java.util.ArrayList;
 
 public class Main
 {
+    private JLabel nameL;
+    private JLabel foodL;
+    private JLabel invitedL;
+
+    private JTextField nameF;
+    private JTextField foodF;
+
+    private JCheckBox invitedBox;
+
+    private JButton addButton;
+    private JButton reportButton;
+
+    private ArrayList<Friend> friendList;
+
     public static void main(String[] args)
     {
-        ArrayList<Friend> friendList;
 
         JOptionPane.showMessageDialog(null, "Welcome to the Party Picker.\n\n" +
                 "Enter person's name and the type of food they're likely to bring, \n" +
                 "then indicate whether they are actually invited to the party or not.");
-
-        friendList = new ArrayList<Friend>();
-
     }
 }
